8d13334d4fce7110a95116aa9471c4011f1ebc09
pufan jiang <jiangpufan@gmail.com>
2017-02-19 01:24:20 +0800

    Refactor repeated code.

--- dailygoal.js
+++ dailygoal.js
@@ -6,73 +6,38 @@ var dgHeader = new Vue({
   }
 })
 
-// Initialize component of daily goal list before user input.
-Vue.component('daily-goal-wait-1', {
-  template:
-  '<div class="row my-1">'
-  + '  <input class="form-control mb-2 col-12 offset-0 " type="text" placeholder="Add a daily goal...">'
-  + '</div>'
-})
-Vue.component('daily-goal-wait-2', {
-  template:
-  '<div class="row my-1">'
-  + '  <input class="form-control mb-2 col-11 offset-1 " type="text" placeholder="Add a daily goal...">'
-  + '</div>'
-})
-Vue.component('daily-goal-wait-3', {
-  template:
-  '<div class="row my-1">'
-  + '  <input class="form-control mb-2 col-10 offset-2 " type="text" placeholder="Add a daily goal...">'
-  + '</div>'
-})
-Vue.component('daily-goal-wait-4', {
-  template:
-  '<div class="row my-1">'
-  + '  <input class="form-control mb-2 col-9 offset-3 " type="text" placeholder="Add a daily goal...">'
-  + '</div>'
-})
+// Initialize component of daily goal list.
 
-Vue.component('daily-goal-active-1', {
-  template:
-  '<div class="row my-1">'
-  + '  <input class="form-control mb-2 col-12 offset-0 " type="text" placeholder="Add a daily goal...">'
-  + '  <div class="w-100"></div>'
-  + '  <button type="button" class="btn btn-success col-1 offset-0 align-self-start mr-2">Save</button>'
-  + '  <button type="button" class="btn btn-danger col-1 align-self-start">Discard</button>'
-  + '</div>'
-})
+var registerWaitComponent = function(order, width, offset) {
+  Vue.component(`daily-goal-wait-${order}`, {
+    template:
+    `<div class="row my-1">
+      <input class="form-control mb-2 col-${width} offset-${offset} " type="text" placeholder="Add a daily goal...">
+    </div>`
+  })
+}
 
-Vue.component('daily-goal-active-2', {
-  template:
-  '<div class="row my-1">'
-  + '  <input class="form-control mb-2 col-11 offset-1 " type="text" placeholder="Add a daily goal...">'
-  + '  <div class="w-100"></div>'
-  + '  <button type="button" class="btn btn-success col-1 offset-1 align-self-start mr-2">Save</button>'
-  + '  <button type="button" class="btn btn-danger col-1 align-self-start">Discard</button>'
-  + '</div>'
-})
+registerWaitComponent(1, 12, 0)
+registerWaitComponent(2, 11, 1)
+registerWaitComponent(3, 10, 2)
+registerWaitComponent(4, 9, 3)
 
-Vue.component('daily-goal-active-3', {
-  template:
-  '<div class="row my-1">'
-  + '  <input class="form-control mb-2 col-10 offset-2 " type="text" placeholder="Add a daily goal...">'
-  + '  <div class="w-100"></div>'
-  + '  <button type="button" class="btn btn-success col-1 offset-2 align-self-start mr-2">Save</button>'
-  + '  <button type="button" class="btn btn-danger col-1 align-self-start">Discard</button>'
-  + '</div>'
-})
+var registerActiveComponent = function(order, width, offset) {
+  Vue.component(`daily-goal-active-${order}`, {
+    template:
+    `<div class="row my-1">
+      <input class="form-control mb-2 col-${width} offset-${offset} " type="text" placeholder="Add a daily goal...">
+      <div class="w-100"></div>
+      <button type="button" class="btn btn-success col-1 offset-${offset} align-self-start mr-2">Save</button>
+      <button type="button" class="btn btn-danger col-1 align-self-start">Discard</button>
+    </div>`
+  })
+}
 
-Vue.component('daily-goal-active-4', {
-  template:
-  '<div class="row my-1">'
-  + '  <input class="form-control mb-2 col-9 offset-3 " type="text" placeholder="Add a daily goal...">'
-  + '  <div class="w-100"></div>'
-  + '  <button type="button" class="btn btn-success col-1 offset-3 align-self-start mr-2">Save</button>'
-  + '  <button type="button" class="btn btn-danger col-1 align-self-start">Discard</button>'
-  + '</div>'
-})
-
-
+registerActiveComponent(1, 12, 0)
+registerActiveComponent(2, 11, 1)
+registerActiveComponent(3, 10, 2)
+registerActiveComponent(4, 9, 3)
 
 // Initialize
 new Vue({
